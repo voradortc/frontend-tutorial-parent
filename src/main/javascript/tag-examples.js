d4e7be99987a352046bb03c3d3ecbb890f2691bc
Victor Ortiz <victor.d.ortiz.-nd@disney.com>
2020-07-15 08:15:04 -0500

    Renormalizing line endings

--- src/main/javascript/tag-examples.js
+++ src/main/javascript/tag-examples.js
@@ -1,5 +1,1 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom";
-import TagsApp from "./components/tags-app";
-
-window.addEventListener( "load", () => ReactDOM.render( <TagsApp/>, document.getElementById( "tags-entry" ) ) );
+import * as React from "react";import * as ReactDOM from "react-dom";import TagsApp from "./components/tags-app";window.addEventListener( "load", () => ReactDOM.render( <TagsApp/>, document.getElementById( "tags-entry" ) ) );
